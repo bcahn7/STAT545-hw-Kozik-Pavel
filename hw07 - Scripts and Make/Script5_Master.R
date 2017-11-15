d9b71f3647e727bd01451a5291e439adfa311c11
Pavel Kozik <pavelkozik@gmail.com>
2017-11-14 18:03:27 -0800

    R Studio

--- hw07 - Scripts and Make/Script5_Master.R
+++ hw07 - Scripts and Make/Script5_Master.R
@@ -1,5 +1,4 @@
 #Master Script
-setwd("C:/Users/Pavel/Desktop/STAT545-hw-Kozik-Pavel/hw07 - Scripts and Make")
 source("Script1_Download.R")
 source("Script2_DataCleaning_and_prep.R")
 source("Script3_Visualization.R")
